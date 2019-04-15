b59bd2c908cb68af29b0e63f70b9ddb934d17087
Jessy Mangat <jessymangat22@hotmail.com>
2019-04-15 16:49:54 -0400

    added styling for home page, button functionality to follow

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="wrapper">
+      <div className="jumbotron jumbotron-fluid">
+  <div className="container hero-text">
+      <div className="col-12">
+    <h1 className="display-4">Musical Weather App</h1>
+    <p>The perfect song for the weather outside.</p>
+    </div>
+  </div>
+  <div className="container">
+  <div className="form-group col-12">
+    <input type="city" className="form-control" id="cityInput" aria-describedby="cityHelp" placeholder="City"/>
+  </div>
+  <div className="form-group col-12">
+    <input type="text" className="form-control" id="provinceInput" aria-describedby="provinceHelp" placeholder="Province / State"/>
+  </div>
+  <div className="form-group col-12">
+    <input type="text" className="form-control" id="countryInput" aria-describedby="countryHelp" placeholder="Country"/>
+  </div>
+  <div className="col-12">
+  <button type="submit" className="btn btn-primary location-button">Get My Weather</button>
+  </div>
+  </div>
+</div>
+</div>
+         
     );
   }
-}
+} 
+
+
 
 export default App;
+
+// Dark sky API key: https://api.darksky.net/forecast/ce78886966dcf6660a6c4074d0de2700/[latitude],[longitude]
+// Google geolocation API : https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBc3yXn2btOPZQ6GjyFbgfW8w9lDCZ1vMg
